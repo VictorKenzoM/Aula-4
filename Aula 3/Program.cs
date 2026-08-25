b9404689253347e61666595bcc5fcfac2f3a0e78
Victor Kenzo <victorkmikado709@gmail.com>
2026-08-25 20:36:59 -0300

    Update Program.cs

--- Aula 3/Program.cs
+++ Aula 3/Program.cs
@@ -30,30 +30,98 @@
             Console.WriteLine("Reprovado");
         }
 
-          Console.WriteLine("Fale 1 a 7: ");
-        string Dia = Console.ReadLine();
 
-     switch (Dia) { 
+        Console.Write("Digite um número de 1 a 7: ");
+        int numero = int.Parse(Console.ReadLine());
 
-        case "1":
-                string = Dia = Console.Read("SEGUNDA");
-             break;
-
-
-            case "dom":​
-
-    Console.WriteLine("Fim de semana");​
-
-    break;​
-
-    default:​
-
-    Console.WriteLine("Dia util");​
-
-    break;​
-
-}​
+        switch (numero)
+        {
+            case 1:
+                Console.WriteLine("Domingo");
+                break;
+            case 2:
+                Console.WriteLine("Segunda-feira");
+                break;
+            case 3:
+                Console.WriteLine("Terça-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 7:
+                Console.WriteLine("Sábado");
+                break;
+            default:
+                Console.WriteLine("Número inválido");
+                break;
+        }
+    }
+}
 
         Console.ReadLine();
     }
+
+
+        Console.Write("Digite o código HTTP: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        string mensagem = codigo switch
+        {
+            200 => "OK - Requisição bem-sucedida",
+            201 => "Criado - Recurso criado com sucesso",
+            400 => "Erro 400 - Requisição inválida",
+            401 => "Erro 401 - Não autorizado",
+            404 => "Erro 404 - Página não encontrada",
+            500 => "Erro 500 - Erro interno do servidor",
+            _ => "Código HTTP desconhecido"
+        };
+
+        Console.WriteLine(mensagem);
+    }
+
+    {
+        Console.Write("Digite seu peso em kg: ");
+        double peso = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite sua altura em metros: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+
+        Console.WriteLine("Seu IMC é: " + imc.ToString("F2"));
+
+        if (imc < 18.5)
+        {
+            Console.WriteLine("Abaixo do peso");
+        }
+        else if (imc < 25)
+        {
+            Console.WriteLine("Peso normal");
+        }
+        else if (imc < 30)
+        {
+            Console.WriteLine("Sobrepeso");
+        }
+        else if (imc < 35)
+        {
+            Console.WriteLine("Obesidade grau I");
+        }
+        else if (imc < 40)
+        {
+            Console.WriteLine("Obesidade grau II");
+        }
+        else
+        {
+            Console.WriteLine("Obesidade grau III");
+        }
+    }
+}
+
+
 }
